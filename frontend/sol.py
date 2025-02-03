b91270d5daee20dab5a4f11b6391b1bc5324ca42
niklas168 <niklaskoenigsdorff@icloud.com>
2025-02-03 19:05:48 +0100

    creating frontend

---
+++ frontend/sol.py
@@ -0,0 +1,94 @@
+import os
+from pathlib import Path
+
+import dotenv
+import requests
+import pandas as pd
+
+import solara
+
+data = {
+    "Recipe Name": ["Pancakes", "Spaghetti Carbonara", "Guacamole", "Chicken Curry"],
+    "Ingredients": [
+        "Flour, Milk, Eggs, Sugar, Baking Powder, Salt",
+        "Spaghetti, Eggs, Parmesan Cheese, Pancetta, Black Pepper, Salt",
+        "Avocado, Lime Juice, Onion, Tomato, Cilantro, Salt",
+        "Chicken, Curry Powder, Coconut Milk, Onion, Garlic, Ginger, Tomato"
+    ]
+}
+
+recipe_name = solara.reactive("")
+ingredients = solara.reactive([])
+ingredient= solara.reactive("")
+
+df=solara.reactive(pd.DataFrame(columns=["Recipe Name"]))
+all_meals=df.value["Recipe Name"].to_list()
+meals=solara.reactive("select meals ...")
+
+@solara.component
+def Page():
+
+    show_dialog = solara.use_reactive(False)
+
+    def load_meals():
+
+        url=os.getenv("BACKEND_HOST")+"/recipe"
+        response=requests.get(url=url)
+
+        result = []
+        for dict_item in response.json():
+            result.append(dict_item["name"])
+
+        df.value=pd.DataFrame(result, columns=["Recipe Name"])
+
+    def add_ingredient():
+        # Append a new reactive value to the ingredients list
+        ingredients.value=ingredients.value+[ingredient.value]
+        ingredient.value=""
+
+    def add_recipe_to_db():
+        #TODO: make conn to db and add recipe here
+        body={
+            "recipe_name": recipe_name.value,
+            "ingredients": ingredients.value,
+        }
+
+        url = os.getenv("BACKEND_HOST")+"/recipe"
+        response=requests.post(url=url, json=body)
+
+        #resetting inputs
+        ingredients.value=[]
+        recipe_name.value=""
+        load_meals()
+
+    def add_items_to_groceries():
+        # todo: take meal.value and put it onto grocery list
+        meals.value=[]
+        pass
+
+    def delete_recipe(column, row_index):
+        #TODO: delete recipe name = df of row x column y
+        pass
+
+    dotenv.load_dotenv()
+    load_meals()
+    with solara.lab.Tabs():
+        with solara.lab.Tab("Rezeptverwaltung"):
+            with solara.Card():
+                #solara.Style(css)
+                cell_actions = [solara.CellAction(icon="mdi-delete", name="Delete Recipe", on_click=delete_recipe)]
+                with solara.Column():
+                    # TODO: get all recipes in the database here
+                    solara.DataFrame(df.value, cell_actions=cell_actions)
+                    solara.Button("Add recipe", on_click=lambda: show_dialog.set(True),classes=["button-1"])
+
+            with solara.lab.ConfirmationDialog(title="Adding new recipe", on_ok=add_recipe_to_db, open=show_dialog):
+                solara.InputText("Recipe Name", value=recipe_name, autofocus=True)
+                solara.InputText("Ingredients", value=ingredient, autofocus=True)
+                solara.Button("Add", on_click=add_ingredient)
+                solara.Markdown(f"Ingredients: {ingredients.value}")
+
+        with solara.lab.Tab("Einkaufslistenvewaltung"):
+            with solara.Card():
+                solara.SelectMultiple(label="Gerichte", values=meals.value, all_values=all_meals)
+                solara.Button(label="Add recipes to grocery list", on_click=add_items_to_groceries)
